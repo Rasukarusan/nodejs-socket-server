56ff3dcdf03703b0f3f7c455e539a45750870973
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-10-25 20:41:35 +0900

    ファイル変更検知を実装

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
+const chokidar = require('chokidar');
 const http = require('http')
 const express = require('express');
 const app = express();
-
-// Socket connection
+const port = 6002
+let globalSocket;
 
 /* Creates new HTTP server for socket */
 const socketServer = http.createServer(app);
@@ -14,25 +15,34 @@ const io = require('socket.io')(socketServer, {
   }
 });
 
-/* Listen for socket connection on port 6002 */
-socketServer.listen(6002, function(){
+// ソケットサーバー立ち上げ
+socketServer.listen(port, function(){
   console.log('Socket server listening on : 6002');
 });
 
-/* This event will emit when client connects to the socket server */
+// ソケット接続確立
 io.on('connection', function(socket){
   console.log('Socket connection established');
-
+  globalSocket = socket
+  // @MEMO: ファイル変更検知の用途のみならクライアントからメッセージ受信はないのでコメントアウト
   // クライアントからメッセージを受信
-  socket.on('message', function(data){
-    console.log('メッセージ受信:', data)
-    socket.send(data)
-  });
+  // socket.on('message', function(data){
+  //   socket.send(data)
+  // });
 });
 
-/* Create HTTP server for node application */
-const server = http.createServer(app);
+if (process.argv.length < 3) {
+  console.log('\u001b[31m' + '\nError: 監視対象のディレクトリを指定してください' + '\u001b[0m')
+  console.log('\u001b[33m' + 'Usage: yarn dev ~/Desktop/hoge\n' + '\u001b[0m')
+  process.exit(0)
+}
 
-/* Node application will be running on 3000 port */
-server.listen(6000);
+const targetDir = process.argv[2]
+console.log('\u001b[35m' + `target = ${targetDir}\n` + '\u001b[0m')
 
+// ファイル変更検知
+chokidar.watch(targetDir, { ignoreInitial: true }).on('all', (event, path) => {
+  console.log(event, path)
+  // クライアントにファイル変更を通知
+  globalSocket.send('file changed')
+});
